f9c20e8d3fa9f9c10330d52d2571d1d49290d051
Fahad Ayaz <fahadayaz35@gmail.com>
2022-09-27 01:41:58 +0500

    Pixabay API

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import '../../../src/index.css'
+
+const NavBar = () => <AppBar title="PixaBay API" className='mui' />;
+
+export default NavBar;
